79cb02bfb0cdababd021bef12db34c1f2b492c9f
AXeL-dev <contact.axel.dev@gmail.com>
2019-09-15 12:13:51 +0100

    Implemented all basic features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ur-frontend-coding-challenge';
+export class AppComponent implements OnInit {
+
+  repositories: any[] = [];
+  isLoading: boolean = true;
+  private currentPage: number = 1;
+  private repositoriesCreationDate: string = '';
+
+  constructor(private title: Title, private httpClient: HttpClient, private datePipe: DatePipe) {
+    // Set title
+    this.title.setTitle('Latest Github Repositories');
+    // Set repositories creation date
+    const date = new Date(new Date().setDate(new Date().getDate() - 30));
+    this.repositoriesCreationDate = this.datePipe.transform(date, 'yyyy-MM-dd');
+  }
+
+  ngOnInit() {
+    this.loadRepositories();
+  }
+
+  onScroll() {
+    if (!this.isLoading) {
+      this.loadRepositories();
+    }
+  }
+
+  private loadRepositories() {
+    this.isLoading = true;
+    this.getRepositories(
+      this.repositoriesCreationDate,
+      this.currentPage,
+      (repositories) => {
+        this.repositories = this.repositories.concat(repositories.items);
+        console.log(this.repositories);
+        this.isLoading = false;
+      },
+      (error) => {
+        console.log(error);
+        this.isLoading = false;
+      });
+  }
+
+  private getRepositories(date: string, page: number = 1, successCallback: (repositories) => void, errorCallback: (error) => void) {
+    const requestUrl = `https://api.github.com/search/repositories?q=created:>${date}&sort=stars&order=desc&page=${page}`;
+    return this.httpClient.get(requestUrl).subscribe((repositories: any[]) => {
+      this.currentPage++;
+      console.log(requestUrl);
+      successCallback(repositories);
+    }, (error) => {
+      console.log(requestUrl);
+      errorCallback(error);
+    });
+  }
+
+  // stolen from: https://stackoverflow.com/a/3224854
+  dateInterval(dateString) {
+    const date = new Date(dateString);
+    const today = new Date();
+    const diffTime = Math.abs(date.getTime() - today.getTime());
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InfiniteScrollModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
